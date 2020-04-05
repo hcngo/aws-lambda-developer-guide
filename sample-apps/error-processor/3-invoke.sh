ed72c9c521e52c61f28deac916f6fddde52bf5b0
Michael Wunderlich <mwunderl@amazon.com>
2020-04-04 22:26:04 -0700

    event input

--- sample-apps/error-processor/3-invoke.sh
+++ sample-apps/error-processor/3-invoke.sh
@@ -2,7 +2,7 @@
 ERROR_FUNCTION=$(aws cloudformation describe-stack-resource --stack-name error-processor --logical-resource-id randomerror --query 'StackResourceDetail.PhysicalResourceId' --output text)
 
 while true; do
-  aws lambda invoke --function-name $ERROR_FUNCTION --payload '{}' out.json
+  aws lambda invoke --function-name $ERROR_FUNCTION --payload file://event.json out.json
   cat out.json
   echo ""
   sleep 2
--- sample-apps/error-processor/random-error/index.js
+++ sample-apps/error-processor/random-error/index.js
@@ -1,29 +1,36 @@
 var AWSXRay = require('aws-xray-sdk-core')
+var aws = AWSXRay.captureAWS(require('aws-sdk'))
 
 var Chance = require('chance')
+var lambda = new aws.Lambda()
 
-var myFunction = function(event, context, callback) {
+var myFunction = async function(event, context) {
   var chance = new Chance()
   var name = chance.first()
-  var roll = chance.d20()
+  var roll1 = chance.integer({ min: 1, max: 1/event["error-rate"] })
+  var roll2 = chance.integer({ min: 1, max: 1/event["error-rate"] })
+
   var guid = chance.guid()
+  console.log("## EVENT: " + JSON.stringify(event, null, 2))
   console.log("GUID: " + guid)
   console.log("Name: " + name)
-  console.log("Roll: " + roll)
+  console.log("Roll 1: " + roll1)
+  console.log("Roll 2: " + roll2)
 
   AWSXRay.captureFunc('annotations', function(subsegment){
     subsegment.addAnnotation('name', name)
-    subsegment.addAnnotation('roll', roll)
+    //subsegment.addAnnotation('roll1', roll1)
+    //subsegment.addAnnotation('roll2', roll2)
     subsegment.addAnnotation('request_id', context.awsRequestId)
   })
 
-  if (roll < 5) {
+  if (roll1 == roll2) {
     var error = new Error("Bad roll")
     console.log("ERROR")
-    callback(error)
+    throw error
   }
 
-  callback(null, {"result": "Success!"})
+  return await lambda.invoke({ FunctionName: context.functionName, Payload: JSON.stringify(event)}).promise()
 }
 
 exports.handler = myFunction
